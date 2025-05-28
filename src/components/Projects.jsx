3475d738600e0a97d4e888598923d377911d8f17
Sheldon-Billy <sheldonbilly0723@gmail.com>
2025-05-28 17:50:59 +0300

    modified UI

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,7 +11,7 @@ const Projects = () => {
         <div className="max-w-5xl mx-auto px-4">
           <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-600 via-cyan-400 to-blue-600 bg-clip-text text-transparent text-center">
             {" "}
-            Featured Projects
+            Personal Projects
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div className="p-6 rounded-xl border border-white hover:-translate-y-1 hover:border-blue-500 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
