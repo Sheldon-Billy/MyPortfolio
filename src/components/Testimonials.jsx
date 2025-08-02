a0d98192d9f288044360753bb10c0f38befa99cc
Sheldon-Billy <sheldonbilly0723@gmail.com>
2025-08-02 13:54:15 +0300

    MOdified Components

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -176,7 +176,7 @@ const Testimonials = () => {
                                                             <span>{testimonial.retweets}</span>
                                                         </button>
                                                         <button className="flex items-center gap-1 hover:text-blue-300 text-[#c02b2b]">
-                                                            <svg className="w-3 h-3 md:w-4 md:w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                                            <svg className="w-3 h-3  md:w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                                                             </svg>
                                                             <span>{testimonial.likes}</span>
