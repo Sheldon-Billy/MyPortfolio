29ab7d61c4610663992ddd9518684375e279d596
Sheldon-Billy <sheldonbilly0723@gmail.com>
2025-08-04 01:03:18 +0300

    Modified Components

--- src/assets/Pics.js
+++ src/assets/Pics.js
@@ -6,11 +6,13 @@ import Sheldon from "./Sheldon.jpeg"
 import Badge1 from "./awsBudge.png"
 import Badge2 from "./EthicalBudge.png"
 import Badge3 from "./AiAgentsBudge.png"
+import Badge4 from "./aws badge.png"
 import Cert1 from "./sololearn Html.png"
 import Cert2 from "./sololearn Python.png"
 import Cert3 from "./sololearn Ethical.png"
 import Cert4 from "./AI cert.png"
 import Cert5 from "./PyrhonDjango.png"
+import Cert6 from "./AWS AI CERT.png"
 import Me from "./me.jpeg"
 import Babe from "./Babe.jpeg"
 import Simo from "./Simo.jpeg"
@@ -22,5 +24,5 @@ import Leon from "./Leon.jpeg"
 
 
 
-export { Badge1, Badge2, Badge3, Cert1, Cert2, Cert3, Cert4, Cert5, Aidriven, Aidriven2, Dermatology, down, Sheldon }
+export { Badge1, Badge2, Badge3, Badge4, Cert1, Cert2, Cert3, Cert4, Cert5, Cert6, Aidriven, Aidriven2, Dermatology, down, Sheldon }
 export { Me, Comfortine, Babe, Simo, Esther, Leon }
